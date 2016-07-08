5d70d0fc0bf3e173287b5b890126140679db17ee
jeanhyung <jinoohome@naver.com>
2016-07-08 14:41:04 +0900

    User 클래스

--- src/countDown/model/domain/User.java
+++ src/countDown/model/domain/User.java
@@ -2,5 +2,52 @@ package countDown.model.domain;
 
 public class User {
 
+	private	String userName;
+	private	String userPass;
+	private	String nickName;
+	private	String timeScore;
+	
+	public User() {}
+
+	public User(String userName, String userPass, String nickName) {
+		super();
+		this.userName = userName;
+		this.userPass = userPass;
+		this.nickName = nickName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPass() {
+		return userPass;
+	}
+
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getTimeScore() {
+		return timeScore;
+	}
+
+	public void setTimeScore(String timeScore) {
+		this.timeScore = timeScore;
+	}
+	
+	
 }
  
