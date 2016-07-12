4027ec8e6b25353828870d84c7af910e3adeed30
user1 <user1@kH1>
2016-07-12 10:17:09 +0900

    u

--- src/countDown/model/service/RankService.java
+++ src/countDown/model/service/RankService.java
@@ -7,18 +7,19 @@ import countDown.model.domain.AscRank;
 import countDown.model.domain.User;
 
 public class RankService {
+	private User[] ur;
 	//랭킹에 등록// prop에 저장  user의 정보저장
-	public void saveUserRank(User[] users){
+	public void saveUserRank(User user){
 		Properties prop = new Properties();
-		
-		for(User user : users){
-			String key = user.getUserId();
-			String value = user.getUserId() + "," + user.getNickName()+ "," + user.getTimeScore();	
+	
+			String key = user.getCode();
+			String value = user.getUserId() + " : " + user.getNickName()+ ", 걸린 시간 : " + user.getTimeScore();	
+
 			prop.setProperty(key, value);
-		}
-		try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("resource/usersRanking.properties"))){
+
+		try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("src/resource/usersRanking.properties",true))){
 			prop.store(bos, null);
-			System.out.println("저장");
+
 		} catch (NullPointerException e){
 			e.printStackTrace();
 		} catch (Exception e) {
@@ -26,44 +27,43 @@ public class RankService {
 		}
 		return;
 	}
-	//랭킹 보기// prop에서 순위대로 불러오기 //기록 오름차순
+	//랭킹 보기// readRank 에서 순위대로 불러오기 //기록 오름차순
 	public String readUserRank(){
 		Properties prop = new Properties();
 		StringBuilder sb = new StringBuilder();
-		ArrayList<User> list = new ArrayList<User>();
-//		AscRank ar = new AscRank().compare(o1, o2);
+		ArrayList<Object> list = new ArrayList<Object>();
 		
+		String value = null;
+
 		try {
-			prop.load(new FileReader("resource/usersRanking.properties"));
+			prop.load(new FileReader("src/resource/usersRanking.properties"));
 		} catch (NullPointerException e){
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+				
+		Set<String> keys = prop.stringPropertyNames();
+		User[] sr = new User[keys.size()];
+		Iterator<String> keyIter = keys.iterator();
 		
-		list.sort(new AscRank());
-		User[] uArray = new User[list.size()];
-		list.toArray(uArray);
-//		Iterator<User> iter = list.iterator();
-		for (User user : uArray) {
-			sb.append(user + "\n");
+		for(int i = 0; keyIter.hasNext(); i++){
+			String key = keyIter.next();
+			value = prop.getProperty(key);
+			String[] values = value.split(",");
+			sr[i]= new User(values[0],values[1],values[2]);
+			list.add(sr[i]);
+			
 		}
-//		Set<Object> keyset = prop.keySet();
-//		Iterator<Object> keyList = keyset.iterator();
-//		 Vector<String> v = new Vector<String>();
-		 
-//		 while (iter.hasNext()) {
-//			 sb.append(iter.next() + "\n");
-//		        User key = iter.next();
-//		        v.add(key);
-//		 }
-//		        Collections.sort(v);
-		        //1등 : 아이디(key), 닉네임 , 기록 순으로 출력되게  기록을 기준으로 정렬
-//		    for (String key : v) {
-//		        sb.append("1등 : " + key + " = " + prop.getProperty(key) + "\n");
-		    	
-//		    }
-//		    System.out.println(sb);
-		    return new String(sb);
+	
+		list.sort(new AscRank());
+	
+		for (int j = 0; j < sr.length; j++) {
+			sb.append(list.get(j)+ "\n");
+		}
+		
+		
+		return new String(sb);
+		
 	}
 }
