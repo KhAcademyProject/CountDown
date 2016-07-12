eb73f7cbcd695fcf4aa877c8b39ffb11e64f5a9d
rldyd <rldyd3783@naver.com>
2016-07-12 14:29:59 +0900

    메인화면 캐릭터추가

--- src/countDown/view/MainFrame.java
+++ src/countDown/view/MainFrame.java
@@ -2,6 +2,8 @@ package countDown.view;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import javax.imageio.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
@@ -10,25 +12,49 @@ public class MainFrame extends JFrame implements ActionListener{
 	private JButton gameStart;
 	private JButton Reverse;
 	private Font font = new Font("Default", Font.BOLD, 20);
-	private JLabel title, version, pd;
+	private JLabel title, version, pd, ImgBox, ImgBox1;
+	private Container cPane;
+	private ImageIcon img, img1;
+	
 	public MainFrame(){
-		
 		super("1 TO 50 GAME"); 	//제목
 		this.setBounds(new Rectangle(500, 300, 800, 600)); 		// 화면크기
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE); 		//닫기
+		Container contentPane = getContentPane();
+		contentPane.setBackground(Color.ORANGE);
+	
+		contentPane.setLayout(new FlowLayout());
 		this.setResizable(false);
-		this.setLocationRelativeTo(null);
-
-
+		
+		cPane = getContentPane();
+		cPane.setLayout(null);
+		
+		img = new ImageIcon("C:/Users/rldyd/Desktop/움1.gif");
+		ImgBox = new JLabel(img);
+		ImgBox.setBounds(500, 80, img.getIconWidth(), img.getIconHeight());
+		ImgBox.setSize(300, 370);
+		
+		cPane.add(ImgBox);
+		
+		img1 = new ImageIcon("C:/Users/rldyd/Desktop/움3.gif");
+		ImgBox1 = new JLabel(img1);
+		ImgBox1.setBounds(20, 80, img.getIconWidth(), img1.getIconHeight());
+		ImgBox1.setSize(275, 370);
+		
+		cPane.add(ImgBox1);
+		
 		Container con = this.getContentPane();
 		con.setLayout(null);
 		
+
 		gameStart = new JButton("Game START");
 		gameStart.setBounds(300, 250, 180, 50);
+		gameStart.setBackground(Color.ORANGE);
 		gameStart.setFont(font);
 
 		Reverse = new JButton("Reverse mode");
 		Reverse.setBounds(300, 350, 180, 50);
+		Reverse.setBackground(Color.orange);
 		Reverse.setFont(font);
 		
 		title = new JLabel("1 to 50");
@@ -43,6 +69,7 @@ public class MainFrame extends JFrame implements ActionListener{
 		pd.setBounds(650, 500, 200, 50);
 		pd.setFont(new Font("Default", Font.PLAIN, 18));
 		
+
 		con.add(gameStart);
 		con.add(Reverse);
 		con.add(title);
@@ -66,7 +93,6 @@ public class MainFrame extends JFrame implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			this.dispose();
 			switch(e.getActionCommand()){
 			case "Game START":
 				new GameFrame();
